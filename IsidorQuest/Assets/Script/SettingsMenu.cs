cfe99fcf247fb68d78a89f5d288f04a3c632cd30
AnxianZhang <gadanxianzhang@gmail.com>
2023-11-16 15:25:54 +0100

    Revert "Revert "Revert "Paused game finished"""
    This reverts commit 2431f71f4463e277d46d171bbaba004397d47c1c.

--- IsidorQuest/Assets/Script/SettingsMenu.cs
+++ IsidorQuest/Assets/Script/SettingsMenu.cs
@@ -7,14 +7,6 @@ public class Settings : MonoBehaviour
    
     [SerializeField] private AudioMixer mixer;
 
-    private void Update()
-    {
-        if (this.gameObject.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape))
-        {
-            quitSettings();
-        }
-    }
-
     public void setVolume(float volume)
     {
         this.mixer.SetFloat("volume", volume);
