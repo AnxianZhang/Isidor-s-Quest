5129abfed95760539bc6700a5a6a2577e13e1c16
AnxianZhang <gadanxianzhang@gmail.com>
2023-11-30 12:25:29 +0100

    home main title and content (bolloow to page) corrected

--- WebsiteSaeReact/isidor/page/HomeScreen.js
+++ WebsiteSaeReact/isidor/page/HomeScreen.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import BackgroundPicture from '../component/Background';
 import Header from '../component/Header';
-import {Dimensions} from 'react-native';
+import { Dimensions } from 'react-native';
 import Play from "../assets/Play.png";
 import Previous from "../assets/previous.png";
 import ButtonImage from '../component/ButtonImage';
@@ -13,164 +13,167 @@ import { ScrollView } from 'react-native-web';
 import BackgroundGame from "../assets/Background1.png"
 import BackgrounGameSecond from "../assets/Background3.png"
 import { getLanguage } from '../function/languageSelect';
+import useScreenWidthDimention from '../hook/useScreenWidthDimention';
 
 const windowHeight = Dimensions.get('window').height;
-const HomeScreen = ({language})=>{
-    const [selectLanguage, setSelectLanguage] = useState(language); 
+const HomeScreen = ({ language }) => {
+    const [selectLanguage, setSelectLanguage] = useState(language);
     const isFocused = useIsFocused();
     const navigation = useNavigation();
     const [isConnect, setIsConnect] = useState(false);
     const firstItemRef = useRef(null);
-    useEffect(()=>{
+    useEffect(() => {
         getData()
-    },[isFocused])
+    }, [isFocused])
 
-    useEffect(()=>{
+    useEffect(() => {
         setSelectLanguage(getLanguage);
     })
 
-    const getData = async ()=>{
+    const getData = async () => {
         const response = await AsyncStorage.getItem("user");
         const responseJSON = JSON.parse(response);
-        if(responseJSON !== null){
+        if (responseJSON !== null) {
             setIsConnect(responseJSON.isConnect);
         }
         console.log("connexion : " + isConnect);
     }
-    return(
+
+    const windowWidthByHook = useScreenWidthDimention()
+    const gameTitleTextFontSizeStyle = windowWidthByHook > 1350 ? 70 : 50;
+    const SecondPartContainerPaddingHorizontalStyle = windowWidthByHook > 1100 ? windowWidthByHook > 1400 ? windowWidthByHook > 1860 ? "24%" : "20%" : "15%" : 0
+    const [gameDescriptionFontTitleSize, gameDescriptionTextFontSize] = windowWidthByHook > 750? windowWidthByHook > 1610? [45, 30] : [40,25] : [35, 20] 
+
+    return (
         <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContent}>
-        <BackgroundPicture source={BackgroundGame} resize="cover" style={styles.image}>
-            <ScrollView>
-            <Header setIsConnect={setIsConnect} setLanguage={setSelectLanguage} language={selectLanguage}/>
-            <View style={styles.gameTitle}>
-                <Text style={styles.gameTitleText}>Isidor's Quest:</Text>
-                <Text style={styles.gameTitleText}>Chasing the Glow</Text>
-            </View>
-            <View style={styles.containButtonPlay}>
-                <TouchableOpacity onPress={() => {isConnect ? navigation.navigate("Game") : navigation.navigate("Connexion")}}>
-                    <View style={styles.buttonContent}>
-                        <Image source={{ uri: Play }} style={styles.playLogo} />
-                            <Text style={styles.headerText}>{selectLanguage.Home.buttonPlay}</Text>
+            <BackgroundPicture source={BackgroundGame} resize="cover" style={styles.image}>
+                <ScrollView>
+                    <Header setIsConnect={setIsConnect} setLanguage={setSelectLanguage} language={selectLanguage} />
+                    <View style={styles.gameTitle}>
+                        <Text style={StyleSheet.compose(styles.gameTitleText, { fontSize: gameTitleTextFontSizeStyle, })}>Isidor's Quest:{"\n"}Chasing the Glow</Text>
                     </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate("PaymentCard")}>
-                    <View style={styles.buttonContent}>
-                        <Image source={{ uri: Play }} style={styles.playLogo} />
-                            <Text style={styles.headerText}>test</Text>
+                    <View style={styles.containButtonPlay}>
+                        <TouchableOpacity onPress={() => { isConnect ? navigation.navigate("Game") : navigation.navigate("Connexion") }}>
+                            <View style={styles.buttonContent}>
+                                <Image source={{ uri: Play }} style={styles.playLogo} />
+                                <Text style={styles.headerText}>{selectLanguage.Home.buttonPlay}</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate("PaymentCard")}>
+                            <View style={styles.buttonContent}>
+                                <Image source={{ uri: Play }} style={styles.playLogo} />
+                                <Text style={styles.headerText}>test</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate("Cancel")}>
+                            <View style={styles.buttonContent}>
+                                <Image source={{ uri: Play }} style={styles.playLogo} />
+                                <Text style={styles.headerText}>testCancel</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <ButtonImage onPress={() => firstItemRef.current.scrollIntoView()} source={{ uri: Previous }} style={styles.previousLogo} />
                     </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate("Cancel")}>
-                    <View style={styles.buttonContent}>
-                        <Image source={{ uri: Play }} style={styles.playLogo} />
-                            <Text style={styles.headerText}>testCancel</Text>
+                    <View style={StyleSheet.compose(styles.SecondPartContainer, {paddingHorizontal: SecondPartContainerPaddingHorizontalStyle,})}>
+                        <View style={{height: 725}}></View> {/* empty box */}
+                        <View style={styles.GameDescriptionContainer} ref={firstItemRef}>
+                            <Text style={StyleSheet.compose(styles.gameDescriptionTitleText, {fontSize: gameDescriptionFontTitleSize})}>{selectLanguage.Home.gameTitle}</Text>
+                            <Text style={StyleSheet.compose(styles.gameDescription, {fontSize: gameDescriptionTextFontSize})}>{selectLanguage.Home.gameDescPartOne}{"\n\n"}{selectLanguage.Home.gameDescPartTwo}</Text>
+                        </View>
                     </View>
-                </TouchableOpacity>
-                <ButtonImage onPress={() => firstItemRef.current.scrollIntoView()} source={{ uri: Previous }} style={styles.previousLogo} />
-            </View>
-            <View style={styles.SecondPartContainer}>
-            <View style={styles.GameDescriptionContainer} ref={firstItemRef}>
-                <Text style={styles.gameDescriptionTitleText}>{selectLanguage.Home.gameTitle}</Text>
-                <Text style={styles.gameDescriptionFirstPartText}>{selectLanguage.Home.gameDescPartOne}</Text>
-                <Text style={styles.gameDescriptionSecondPartText}>{selectLanguage.Home.gameDescPartTwo}</Text>
-            </View>
-            </View>
-            </ScrollView>
-        </BackgroundPicture>
-        <BackgroundPicture source={BackgrounGameSecond} resize="cover" style={styles.image}>
-
-
-        </BackgroundPicture>
+                </ScrollView>
+            </BackgroundPicture>
+            {/* <BackgroundPicture source={BackgrounGameSecond} resize="cover" style={styles.image}></BackgroundPicture> */}
         </ScrollView>
     )
 }
 const styles = StyleSheet.create({
-scrollView: {
-    flex: 1,
-},
-scrollViewContent: {
-    flexGrow: 1,
-    backgroundColor: "#7094CB"
-},
-image : {
-    width: '100%', 
-    minHeight: 1800,
-},
-gameTitle : {
-    flexDirection: 'column',
-    alignItems: "flex-end",
-    justifyContent : "center",
-    paddingRight: 40,
-    height: windowHeight*0.75,
-},
-gameTitleText:{
-    fontSize: 70,
-    fontFamily:"Regular",
-    color: "white",
-},
-buttonContent: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    backgroundColor : "#EE8A45",
-    paddingTop : 5,
-    paddingBottom : 5,
-    paddingRight : 30,
-    paddingLeft : 30,
-    borderRadius : 10
-},
-playLogo: {
-    width: 25,
-    height: 33,
-    marginRight : 20
-},
-containButtonPlay: {
-    alignItems: "center",
-    justifyContent : "center",
-    height: windowHeight*0.10,
-},
-previousLogo: {
-    width: 25,
-    height: 33,
-},
-GameDescriptionContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: "10%",
-    paddingTop : '40%',
-    marginLeft : '20%'
-  },
-gameDescriptionTitleText:{
-    fontSize : 32,
-    fontFamily : "Bold",
-    color : "#FFFFFF"
-},
-SecondPartContainer: {
-    flex: 1,
-    paddingHorizontal: "3%",
-},
-gameDescriptionFirstPartText:{
-    paddingTop : 20,
-    paddingBottom : 20,
-    fontSize : 19, 
-    fontFamily : "Light", 
-    color : "white",
-    textAlign : "left",
-    paddingRight : 200,
-    paddingLeft : 200
-},
-gameDescriptionSecondPartText:{
-    fontSize : 19, 
-    fontFamily : "Light", 
-    color : "white",
-    paddingRight : 200,
-    textAlign : "left",
-    paddingLeft : 200,
-},
-headerText: {
-    fontSize: 24,
-    fontFamily: "regular",
-    color: "white",
-},
+    scrollView: {
+        flex: 1,
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        backgroundColor: "#7094CB"
+    },
+    image: {
+        width: '100%',
+        minHeight: 2090,
+        // height: "100%"
+    },
+    gameTitle: {
+        flexDirection: 'column',
+        alignItems: "flex-end",
+        justifyContent: "center",
+        // paddingRight: 40,
+        marginHorizontal: 50,
+        height: windowHeight * 0.60,
+        marginVertical: 20
+    },
+    gameTitleText: {
+        fontFamily: "Regular",
+        color: "white",
+        textAlign: 'center',
+    },
+    buttonContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: "#EE8A45",
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingRight: 30,
+        paddingLeft: 30,
+        borderRadius: 10
+    },
+    playLogo: {
+        width: 25,
+        height: 33,
+        marginRight: 20
+    },
+    containButtonPlay: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: windowHeight * 0.10,
+    },
+    previousLogo: {
+        width: 25,
+        height: 33,
+    },
+    GameDescriptionContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 45,
+        // marginTop: 500,
+        // paddingHorizontal: "10%",
+        // paddingTop: '40%',
+        // marginLeft: '20%'
+    },
+    SecondPartContainer: {
+        flex: 1,
+        // paddingHorizontal: "24%",
+    },
+    gameDescriptionTitleText: {
+        // fontSize: 45,
+        fontFamily: "Bold",
+        color: "#FFFFFF",
+        marginVertical: 10
+    },
+    gameDescription: {
+        // paddingTop: 20,
+        // paddingBottom: 20,
+        // fontSize: 30,
+        fontFamily: "Light",
+        color: "white",
+        // marginHorizontal: "25%",
+        marginVertical: 20
+        // textAlign: "left",
+        // paddingRight: 200,
+        // paddingLeft: 200
+    },
+    headerText: {
+        fontSize: 24,
+        fontFamily: "regular",
+        color: "white",
+    },
 });
 
 export default HomeScreen;
