4d8b73d316401c451b549f6412fe3a0083e9e2c3
Oscar Lin <heartzephyr@gmail.com>
2024-01-24 18:31:13 +0100

    WebSocket serveur pour l'affichage de la position des joueurs
    pour lancer le serveur :
    - ouvrir la console
    - se diriger dans le dossier backend
    - utiliser "npm run ws"

---
+++ WebsiteSaeReact/Backend/ws_server.js
@@ -0,0 +1,43 @@
+const WebSocket = require("ws");
+
+const server = new WebSocket.Server({port : 8080});
+
+
+/** Liste d'object json
+ * pseudo : String
+ * character : String (Warrior ou archer)
+ * position : int[2]
+ * currentScene : String
+ */
+const dataPos = [];
+
+server.on("connection", (ws) => {
+
+    ws.on("message", (message) => {
+        try{
+            data = JSON.parse(message);
+            if(data.scene != null){
+                ws.send(searchData(data.scene));
+            }
+            addData(data);
+        }catch (e){
+            ws.send("Not a valid argument");
+        }
+
+    })
+})
+
+function dataIsValid(data){
+    return data.pseudo != null && data.character != null && data.position != null && data.currentScene != null;
+}
+
+function addData(data){
+    if(!dataIsValid(data)){
+        throw new SyntaxError();
+    }
+
+}
+
+function searchData(scene){
+    
+}
